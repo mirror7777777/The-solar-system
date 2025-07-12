172e1f19ca4c2b4d19ff2b2558cefbd194e5278a
George Ajileye <georgeajileye1@gmail.com>
2025-07-12 18:01:36 -0400

    added express commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require ('path')
+const app = express();
+const Port = 8090;
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.listen(Port , ()=>{
+    console.log(`Port is open at: http://localhost:${Port}`)
+})
+app.use(express.static('public'))
+
+app.get('/', (req , res)=>{
+    res.sendFile(path.join(__dirname, 'html/landing.html'))
+
+});
+app.get('/about', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'html/About.html'))
+})
+app.get('/Facts', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'html/facts.html'))
+})
